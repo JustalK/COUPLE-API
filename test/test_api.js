9f08d8cf070413c7760df2b4ed28890e687de864
Latsuj <justal.kevin@gmail.com>
2020-12-14 23:32:15 +0800

    [CONFIG] Adding the config for travis and coveralls

--- test/test_api.js
+++ test/test_api.js
@@ -13,7 +13,7 @@ test.before(async () => {
 	await m_seeding.seed();
 });
 
-test('[STATIC] Testing the checker of the api', async t => {
+test('[STATIC] Testing the get_all_questions_by_level', async t => {
 	const response = await m_utils.getter({ query: `
 		query {
 			get_all_questions_by_level(level: 0) {
@@ -21,8 +21,24 @@ test('[STATIC] Testing the checker of the api', async t => {
 			}
 		}`
 	});
-	
+
 	t.is(response.get_all_questions_by_level.length, 2);
 	t.is(response.get_all_questions_by_level[0].question, 'What do you think about living together ?');
 	t.is(response.get_all_questions_by_level[1].question, 'What do you think about rrrr together ?');
+});
+
+test('[STATIC] Testing the get_all_questions', async t => {
+	const response = await m_utils.getter({ query: `
+		query {
+			get_all_questions {
+				question
+			}
+		}`
+	});
+
+	t.is(response.get_all_questions.length, 4);
+	t.is(response.get_all_questions[0].question, 'What do you think about living together ?');
+	t.is(response.get_all_questions[1].question, 'What do you think about rrrr together ?');
+	t.is(response.get_all_questions[2].question, 'What do you think about dddd together ?');
+	t.is(response.get_all_questions[3].question, 'What do you think about eeee together ?');
 });
